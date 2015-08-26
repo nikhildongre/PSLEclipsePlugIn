00a48c0a6f751bcd857b3646cd61fd338dbd37bf
vishalgupta12 <vishal_gupta1@BLL05602.persistent.co.in>
2015-08-26 15:40:02 +0530

    testing

--- src/main/java/com/psl/pluggin/controller/UserController.java
+++ src/main/java/com/psl/pluggin/controller/UserController.java
@@ -14,8 +14,6 @@ import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
